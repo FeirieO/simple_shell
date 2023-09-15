b1eacb86bbc911b64e92a228974935fcc5d6051a
FeirieO <feirie001@gmail.com>
2023-09-15 02:36:43 -0700

    added file for second builtin help func

---
+++ aux_helper2.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * aux_helper - provide information for the builtin help.
+ * Return: NULL
+ */
+
+void aux_helper(void)
+{
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+/**
+ * aux_helper_alias - provides information for the builtin alias.
+ * Return: NULL
+ */
+
+void aux_helper_alias(void)
+{
+	char *help = "alias: alias [-p] [name[=value]...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+/**
+ * aux_helper_cd - provide information for the builtin alias.
+ * Return: NULL
+ */
+
+void aux_helper_cd(void)
+{
+	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
