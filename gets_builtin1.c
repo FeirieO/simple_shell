0b200dab29cfad787f6e99c3cfae84b711a40d09
Ife-Calis <calistusifechukwu39@gmail.com>
2023-09-20 14:42:51 -0700

    gets_builtin

---
+++ gets_builtin1.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * gets_builtin - for pairing the command in the arg
+ * @cmds: command
+ * Return: Function pointer of the builtin command
+ */
+
+int (*gets_builtin(char *cmds))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int j;
+
+	for (j = 0; builtin[j].name; j++)
+	{
+		if (_strcmp(builtin[j].name, cmds) == 0)
+			break;
+	}
+
+	return (builtin[j].f);
+}
